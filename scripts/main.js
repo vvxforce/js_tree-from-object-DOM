30b45cb80c937449c4ac0bb1cf7247e2dd61f453
vvxforce <vvxforce@gmail.com>
2019-12-03 13:50:07 +0200

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+// 'use strict';
+
+// import { create } from 'domain';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    if (typeof (data[key]) === 'object') {
+      const li = document.createElement('li');
+
+      li.innerText = key;
+      ul.append(li);
+      createTree(ul, data[key]);
+      element.append(ul);
+    }
+  }
+}
+
+createTree(tree, food);
